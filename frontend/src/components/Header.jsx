c2dae4e99d4abcc8ce224335d789dc22aa390f16
DongukKang2 <kkdu7964@gmail.com>
2024-12-16 16:17:58 +0900

    feat: update register page and remove unnecessary fields

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Header.css';
+import "./Header.css";
 
 const HeaderButton = ({ text, link }) => {
-    return (
-        <Link to={link} className="header-button">{text}</Link>
-    );
+  return (
+    <Link to={link} className="header-button">
+      {text}
+    </Link>
+  );
 };
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="header-title">Fintrend</div>
-            <div className="header-content">
-                <HeaderButton text="about" link="/about" />
-                <HeaderButton text="login" link="/login" />
-                <HeaderButton text="register" link="/register" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="header">
+      <Link to="/" className="header-title">
+        Fintrend
+      </Link>
+      <div className="header-content">
+        <HeaderButton text="about" link="/about" />
+        <HeaderButton text="login" link="/login" />
+        <HeaderButton text="register" link="/register" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
