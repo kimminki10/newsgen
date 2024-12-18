132e1b87ce57ff3f29e15c54b44c11d0650a294b
fenpon <fenpon45@naver.com>
2024-12-18 17:31:35 +0900

    스케줄러 스레드 안정성 관련 기능 추가

--- crawling/chrome_driver.py
+++ crawling/chrome_driver.py
@@ -2,6 +2,6 @@ from selenium import webdriver
 
 def get_driver():
     options = webdriver.ChromeOptions()
-    #options.add_experimental_option("detach", True)
+    
     driver = webdriver.Chrome(options=options)
     return driver
