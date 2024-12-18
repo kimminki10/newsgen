11f6d838fd457f0e40d5dcea1a663785778cbe6c
fenpon <fenpon45@naver.com>
2024-12-18 16:07:03 +0900

    중복실행 방지 코드

--- trandlator/apps.py
+++ trandlator/apps.py
@@ -1,12 +1,19 @@
 from django.apps import AppConfig
 from django.conf import settings
+from .jobs_status import is_job_registered, register_job  # 작업 상태 관리 함수 가져오기
 import threading
+import os
+
 
 class TrandlatorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'trandlator'
 
     def ready(self):
+         # 중복 실행 방지 (디버그 모드시 두번 실행됨)
+        if os.environ.get('RUN_MAIN') != 'true':
+            print("Skipping duplicate scheduler initialization")
+            return
         print("Scheduler ready")
         if settings.DEBUG:
             print("Scheduler ready debug")
@@ -19,28 +26,36 @@ class TrandlatorConfig(AppConfig):
         from apscheduler.executors.pool import ThreadPoolExecutor
         from apscheduler.triggers.interval import IntervalTrigger
         from django_apscheduler.jobstores import DjangoJobStore, register_events, register_job
-        
-        Automate()
-        
-        print("Scheduler started")
-        scheduler = BackgroundScheduler({
-            'jobstores': {
-                'default': DjangoJobStore(),
-            },
-            'executors': {
-                'default': ThreadPoolExecutor(20),
-            },
-            'job_defaults': {
-                'coalesce': False,
-                'max_instances': 1,
-            },
-        })
-        register_events(scheduler)
-        scheduler.start()
 
-        # Register the job
- 
-        #@register_job(scheduler, IntervalTrigger(seconds=10))
-        @register_job(scheduler, IntervalTrigger(minutes=5))
-        def scheduled_Automate():
+        #Automate()
+
+       
+       
+        job_id = "automate_job"  # 작업의 고유 ID
+        if not is_job_registered(job_id):
+            register_job(job_id)
+            print("Registering new job...")
+            scheduler = BackgroundScheduler({
+                'jobstores': {
+                    'default': DjangoJobStore(),
+                },
+                'executors': {
+                    'default': ThreadPoolExecutor(20),
+                },
+                'job_defaults': {
+                    'coalesce': False,
+                    'max_instances': 1,
+                },
+            })
+            
+            register_events(scheduler)
+            scheduler.start()
+
             Automate()
+            
+            # Register the job
+            @register_job(scheduler, IntervalTrigger(seconds=10), id=job_id, replace_existing=False)
+            def scheduled_automate():
+                Automate()
+        else:
+            print("Job already registered. Skipping registration.")
