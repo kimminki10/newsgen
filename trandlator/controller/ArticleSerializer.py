dec5bcf760e4ef9d6ad5de29590a611f8dadff6e
johnwon2007 <johnwon2007@gmail.com>
2024-12-19 02:17:51 +0900

    changed or added serializers needed

--- trandlator/controller/ArticleSerializer.py
+++ trandlator/controller/ArticleSerializer.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from trandlator.models import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
+    tickers = serializers.SerializerMethodField()
+    def get_tickers(self, obj):
+        from trandlator.controller.TickerSerialize import TickerSerializer  # Import here
+        return [ticker.ticker_name for ticker in obj.tickers.all()]
     class Meta:
         model = Article
         fields = '__all__'
