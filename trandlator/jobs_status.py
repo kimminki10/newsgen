77ad7ecf95dfb87d08e6a38173edae26a573a150
fenpon <fenpon45@naver.com>
2024-12-19 21:21:53 +0900

    매일 한국 시간 11:30pm(미국장 시작시간) 에 함수 호출하도록 수정
    스케줄러 종료시 처리 추가

--- trandlator/jobs_status.py
+++ trandlator/jobs_status.py
@@ -1,16 +1,26 @@
-# trandlator/job_status.py
-
+import threading
 # 전역 변수로 작업 상태 저장
 registered_jobs = {}
 
+job_id = "automate_job"  # 작업의 고유 ID
+
 def is_job_registered(job_id):
+    global registered_jobs
     """작업이 등록되었는지 확인"""
     return registered_jobs.get(job_id, False)
 
 def register_job(job_id):
+    global registered_jobs
     """작업을 등록"""
     registered_jobs[job_id] = True
 
+def unregister_job(job_id):
+    global registered_jobs
+    """작업을 해제"""
+    if job_id in registered_jobs:
+        del registered_jobs[job_id]
+
 def clear_job_status():
+    global registered_jobs
     """모든 작업 상태 초기화 (테스트용 또는 서버 재시작 시)"""
     registered_jobs.clear()
