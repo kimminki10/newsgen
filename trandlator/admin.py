eb29ec2adb31bf94f89fcccf8f199d36179a5e10
kimminki10 <pangshe10@gmail.com>
2024-12-19 17:27:49 +0900

    fix: add article admin class

--- trandlator/admin.py
+++ trandlator/admin.py
@@ -3,6 +3,10 @@ from .models import Article
 from .models import Ticker
 from .models import User
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'origin_url', 'created_at')
+    search_fields = ('title', 'content')
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Ticker)
 admin.site.register(User)
