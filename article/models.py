bb2956ffcb7177fb33724d34c7dc167bf7a82dde
kimminki10 <pangshe10@gmail.com>
2025-04-04 14:27:08 +0900

    fix: update datetime fields to use timezone.now for better timezone handling

--- article/models.py
+++ article/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
 
 class Article(models.Model):
     title = models.CharField(max_length=100, default='empty title')
     content = models.TextField(default='empty content')
     summary = models.TextField(default='empty summary')
-    created_at = models.DateTimeField(default=datetime.now, blank=True)
-    updated_at = models.DateTimeField(default=datetime.now, blank=True)
+    created_at = models.DateTimeField(default=timezone.now, blank=True)
+    updated_at = models.DateTimeField(default=timezone.now, blank=True)
     views = models.PositiveIntegerField(default=0)
     origin_url = models.URLField(max_length=200, blank=True, unique=True)
     tts_content = models.TextField(default='empty tts content')
--- ticker/models.py
+++ ticker/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
 from article.models import Article
 
 
@@ -10,8 +10,8 @@ class Ticker(models.Model):
     before_last_price = models.FloatField(default=0)
     price_diff = models.FloatField(default=0)
     percentage_diff = models.FloatField(default=0)
-    last_price_date = models.DateTimeField(default=datetime.now, blank=True)
-    before_last_date = models.DateTimeField(default=datetime.now, blank=True)
+    last_price_date = models.DateTimeField(default=timezone.now, blank=True)
+    before_last_date = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
         return self.ticker_name
